d2ae5106889192433d431c996b4264312303d89c
Anurag Joshi <4219620+aj100@users.noreply.github.com>
2018-03-14 19:27:57 -0400

    Fix broken angular files.

--- src/client/app/samples/eth/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var angular = require('angular');
-var eth_controller_1 = require("./eth.controller");
-exports.default = angular.module('recLettersApp.eth', [])
-    .controller('EthController', eth_controller_1.default)
-    .name;
