88efabf8c811820f370de1fb8de2051c9cb6f581
Ying Wang <yingtianwang@g.harvard.edu>
2018-03-23 13:31:59 -0700

    update README and start.sh

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cscie599bc
 Blockchain project
+
+1. Make sure you have geth or ganache running, with the same url and port set up in src/LetterContract/truffle.js
+2. run `./src/start.sh` to start the service
---
+++ src/start.sh
@@ -0,0 +1,14 @@
+# enter truffle folder to run truffle commands
+cd LetterContract;
+# deleted previously compiled files
+rm -rf /build/*;
+# compile contracts
+truffle compile;
+# deploy contracts, make sure you connected to geth or ganache
+truffle migrate;
+# start ipfs
+ipfs daemon &
+# start mongod
+mongod &
+# start web server
+gulp serve
