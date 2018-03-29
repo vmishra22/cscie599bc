25a7a29ee2571542d75ea7f1a23f5e0727e585bb
Kevin Sun <sunkev@brandeis.edu>
2018-03-29 17:11:10 -0400

    student dashboard without link

--- src/client/app/dashboards/student-dash/student-dash.controller.ts
+++ src/client/app/dashboards/student-dash/student-dash.controller.ts
@@ -1,5 +1,26 @@
 'use strict';
 
+import { RecLetter } from '../../classes/recletter';
+
 export default class StudentDashController {
-  constructor() { }
+  $http;
+  $location;
+  recLetters: RecLetter[];
+
+  /*@ngInject*/
+  constructor($http, $location) {
+    this.$http = $http;
+    this.$location = $location;
+  }
+
+  $onInit() {
+    this.setRecommendations(this);
+  }
+
+  setRecommendations($scope) {
+    this.$http.get('http://localhost:3000/api/RecommendationLetters')
+      .then(function(response) {
+        $scope.recommendations = response.data;
+      });
+  }
 }
