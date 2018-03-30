a698ab79e3bc02a74562a3827e549bc58a55127d
Kevin Sun <sunkev@brandeis.edu>
2018-03-30 11:48:46 -0400

    change dashboard routes and add recommendar dashboard tables

--- src/client/app/dashboards/dashboards.routes.ts
+++ src/client/app/dashboards/dashboards.routes.ts
@@ -4,19 +4,19 @@ export default function routes($stateProvider) {
   'ngInject';
   $stateProvider
     .state('student-dash', {
-      url: '/dashboard',
+      url: '/student-dashboard',
       template: require('./student-dash/student-dash.html'),
       controller: 'StudentDashController',
       controllerAs: 'vm'
     })
     .state('school-dash', {
-      url: '/dashboard',
+      url: '/school-dashboard',
       template: require('./school-dash/school-dash.html'),
       controller: 'SchoolDashController',
       controllerAs: 'vm'
     })
     .state('rec-dash', {
-      url: '/dashboard',
+      url: '/recommender-dashboard',
       template: require('./rec-dash/rec-dash.html'),
       controller: 'RecDashController',
       controllerAs: 'vm'
--- src/client/app/dashboards/rec-dash/red-dash.controller.ts
+++ src/client/app/dashboards/rec-dash/red-dash.controller.ts
@@ -1,5 +1,27 @@
 'use strict';
 
-export default class RecDashController {
-  constructor() { }
+import { RecLetter } from '../../classes/recletter';
+
+export default class RedDashController {
+  $http;
+  $location;
+  recLetters: RecLetter[];
+
+  /*@ngInject*/
+  constructor($http, $location) {
+    this.$http = $http;
+    this.$location = $location;
+  }
+
+  $onInit() {
+    this.setRecommendations(this);
+  }
+
+  setRecommendations($scope) {
+    this.$http.get('http://localhost:3000/api/RecommendationLetters')
+      .then(function(response) {
+        $scope.pendingRecs = response.data;
+        $scope.submittedRecs = response.data;
+      });
+  }
 }
