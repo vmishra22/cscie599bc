cd9d2235fd084d3c3293a70b6504c9c5006bd206
Anurag Joshi <4219620+aj100@users.noreply.github.com>
2018-04-10 19:57:07 -0400

    Updated start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,15 +1,17 @@
 # enter truffle folder to run truffle commands
 cd LetterContract;
 # deleted previously compiled files
-rm -rf /build/*;
+rm -rf build;
 # compile contracts
 truffle compile;
 # deploy contracts, make sure you connected to geth or ganache
 truffle migrate;
+# Go back to src
+cd ..;
 # start ipfs
 #ipfs daemon &
 # start mongod
 mongod &
-# mongo recletters-dev --eval "db.recletterrequests; db.recletterrequests.deleteMany({}); db.recletters.deleteMany({}); db.degreeprograms.deleteMany({}); db.candidatequestions.deleteMany({}); quit();"
+mongo recletters-dev --eval "db.recletterrequests; db.recletterrequests.deleteMany({}); db.recletters.deleteMany({}); quit();"
 # start web server
 gulp serve
