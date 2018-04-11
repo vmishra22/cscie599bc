6daca5862125d229ef4aff0c464a0ce88780b777
Anurag Joshi <4219620+aj100@users.noreply.github.com>
2018-04-10 21:27:59 -0400

    Cleaning up seed data.

--- src/server/config/seed.js
+++ src/server/config/seed.js
@@ -4,7 +4,6 @@
  */
 
 'use strict';
-import Thing from '../api/thing/thing.model';
 import User from '../api/user/user.model';
 import config from './environment/';
 
@@ -12,223 +11,130 @@ import CandidateQuestion from '../model/candidatequestions';
 import DegreeProgram from '../model/degreeprograms';
 
 export default function seedDatabaseIfNeeded() {
+  //Sample schools
+  const schools = [
+    {
+      provider: 'local',
+      name: "Harvard",
+      email: "harvard@harvard.edu",
+      addressLine1: "123 College",
+      city: "Cambridge",
+      state: "MA",
+      zip: "12345",
+      schoolContactName: "John Harvard",
+      schoolContactNumber: "(123) 456-7890",
+      role : "school",
+      password: "test"
+    },
+
+    {
+      provider: 'local',
+      name: "Stanford",
+      email: "standford@stanford.edu",
+      addressLine1: "...",
+      city: "...",
+      state: "...",
+      zip: "...",
+      schoolContactName: "...",
+      schoolContactNumber: "(123) 456-7890",
+      role : "school",
+      password: "test"
+    },
+
+    {
+      provider: 'local',
+      name: "MIT",
+      email: "mit@mit.edu",
+      addressLine1: "123 College",
+      city: "...",
+      state: "...",
+      zip: "...",
+      schoolContactName: "...",
+      schoolContactNumber: "(123) 456-7890",
+      role : "school",
+      password: "test"
+    },
+    
+    {
+      provider: 'local',
+      name: "NYU",
+      email: "nyu@nyu.edu",
+      addressLine1: "123 College",
+      city: "...",
+      state: "...",
+      zip: "...",
+      schoolContactName: "...",
+      schoolContactNumber: "(123) 456-7890",
+      role : "school",
+      password: "test"
+    },
+
+    {
+      provider: 'local',
+      name: "Waterloo",
+      email: "waterloo@waterloo.edu",
+      addressLine1: "123 College",
+      city: "...",
+      state: "...",
+      zip: "...",
+      schoolContactName: "...",
+      schoolContactNumber: "(123) 456-7890",
+      role : "school",
+      password: "test"
+    },
+  ];
+
+  //Sample questions
+  const completeQuestions = [
+    {
+      questionText: 'In what capacity have you known the applicant?',
+      responseChoices: [ 
+        'Large Class / Lecture',
+        'Small Class / Seminar/ Lab',
+        'Independent Study Student', 
+        'Advisee','Other'
+      ]
+    },
+    {
+      questionText: 'How well do you know the applicant?',
+      responseChoices: [
+        'See on a regular basis',
+        'At one time',
+        'Knew him/her only through in-class contact',
+        'Only through records'
+      ]
+    }
+  ];
+  const percentileQuestions = [
+    'What is your overall ranking of this student compared to other students?',
+    'Analytical ability',
+    'Quantitative ability',
+    'Ability in oral expression',
+    'Analytical in written expression',
+    'Imagination and Creativity',
+    'Ability to work with others',
+    'Flexibility',
+    'Maturity',
+    'Confidence',
+    'Leadership',
+    'Motivation for proposed program of study',
+    'Background for proposed program of study'
+  ].map(q => {
+    return {
+      questionText: q,
+      responseChoices: [ 'Upper 50%', 'Lower 50%', 'Upper 25%', 'Upper 10%', 'No basis for judgment']
+    }
+  });
+  const questions = completeQuestions.concat(percentileQuestions)
+
+  //Sample Degree Programs
   if(config.seedDB) {
-    Thing.find({}).remove()
-      .then(() => {
-        let thing = Thing.create({
-          name: 'Development Tools',
-          info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-                + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-                + 'Stylus, Sass, and Less.'
-        }, {
-          name: 'Server and Client integration',
-          info: 'Built with a powerful and fun stack: MongoDB, Express, '
-                + 'AngularJS, and Node.'
-        }, {
-          name: 'Smart Build System',
-          info: 'Build system ignores `spec` files, allowing you to keep '
-                + 'tests alongside code. Automatic injection of scripts and '
-                + 'styles into your app.html'
-        }, {
-          name: 'Modular Structure',
-          info: 'Best practice client and server structures allow for more '
-                + 'code reusability and maximum scalability'
-        }, {
-          name: 'Optimized Build',
-          info: 'Build process packs up your templates as a single JavaScript '
-                + 'payload, minifies your scripts/css/images, and rewrites asset '
-                + 'names for caching.'
-        }, {
-          name: 'Deployment Ready',
-          info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-                + 'and openshift subgenerators'
-        });
-        return thing;
-      })
-      .then(() => console.log('finished populating things'))
-      .catch(err => console.log('error populating things', err));
+    const schoolEmails = schools.map(s => s.email );
+    User.remove({email: {$in: schoolEmails}})
+      .then(() => User.create(schools))
+      .catch(err => console.log('Error creating schools: ', err));
 
-      User.remove({email: 'test@example.com'})
-      .then(() => {
-        User.create({
-          provider: 'local',
-          role: 'student',
-          name: 'Test User',
-          email: 'test@example.com',
-          password: 'test'
-        })
-        .catch(err => console.log('error populating users', err));
-      });
-
-      User.remove({email: 'admin@example.com'})
-      .then(() => {
-        User.create({
-          provider: 'local',
-          role: 'admin',
-          name: 'Admin',
-          email: 'admin@example.com',
-          password: 'admin'
-        })
-        .then(() => console.log('finished populating users'))
-        .catch(err => console.log('error populating users', err));
-      });
-      
     CandidateQuestion.find({}).remove()
-    .then(() => {
-      CandidateQuestion.create({
-        questionText: 'In what capacity have you known the applicant?',
-        responseChoices: [
-          'Large Class / Lecture',
-          'Small Class / Seminar/ Lab',
-          'Independent Study Student',
-          'Advisee',
-          'Other'
-        ]
-      },
-      {
-        questionText: 'How well do you know the applicant?',
-        responseChoices: [
-          'See on a regular basis',
-          'At one time',
-          'Knew him/her only through in-class contact',
-          'Only through records'
-        ]
-      },
-      {
-        questionText: 'What is your overall ranking of this student compared to other students?',
-        responseChoices: [
-          'Upper 50%',
-          'Lower 50%',
-          'Upper 25%',
-          'Upper 10%',
-          'No basis for judgment'
-        ]
-      },
-      {
-        questionText: 'Analytical ability',
-        responseChoices: [
-          'Upper 50%',
-          'Lower 50%',
-          'Upper 25%',
-          'Upper 10%',
-          'No basis for judgment'
-        ]
-      },
-      {
-        questionText: 'Quantitative ability',
-        responseChoices: [
-          'Upper 50%',
-          'Lower 50%',
-          'Upper 25%',
-          'Upper 10%',
-          'No basis for judgment'
-        ]
-      },
-      // {
-      //   questionText: 'Ability in oral expression',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Analytical in written expression',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Imagination and Creativity',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Ability to work with others',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Flexibility',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Maturity',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Confidence',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Leadership',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      // {
-      //   questionText: 'Motivation for proposed program of study',
-      //   responseChoices: [
-      //     'Upper 50%',
-      //     'Lower 50%',
-      //     'Upper 25%',
-      //     'Upper 10%',
-      //     'No basis for judgment'
-      //   ]
-      // },
-      {
-        questionText: 'Background for proposed program of study',
-        responseChoices: [
-          'Upper 50%',
-          'Lower 50%',
-          'Upper 25%',
-          'Upper 10%',
-          'No basis for judgment'
-        ]
-      })
-      .then(() => console.log('finished populating candidate questions'))
-      .catch(err => console.log('error populating candidate questions', err));
-    });
-
-    // DegreeProgram.find({}).remove()
-    // .then(() => {});
+      .then(() => CandidateQuestion.create(questions));
   }
 }
