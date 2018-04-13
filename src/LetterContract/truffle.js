9abfffb7f56a5fc111dae6ed00ed110f9c22924a
Vinay Mishra <vinay.mishra@3ds.com>
2018-04-13 14:56:18 -0400

    coverage related

--- src/LetterContract/truffle.js
+++ src/LetterContract/truffle.js
@@ -6,7 +6,8 @@ module.exports = {
       port: 7545,
       network_id: '5777', // Match any network id
       gas: 3500000
-    }
+    },
+    coverage: { host: "127.0.0.1", network_id: "*", port: 8555, gas: 0xfffffffffff, gasPrice: 0x01 }
   },
   rpc: {
         host: 'localhost',
